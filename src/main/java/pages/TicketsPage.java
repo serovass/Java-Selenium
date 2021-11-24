0dfd015fcbbdd607832a5b6a84ba8130bef9caac
Gregorio Naida <gnaida@oa-l5.msk.21-school.ru>
2021-11-24 16:16:19 +0300

    ticketsPage

--- src/main/java/pages/TicketsPage.java
+++ src/main/java/pages/TicketsPage.java
@@ -1,6 +1,30 @@
 package pages;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+
 
 public class TicketsPage extends AbstractPage {
 
+    private WebElement queue = driver.findElement(By.xpath("//*[@id=\"id_queue\"]"));
+    private WebElement queueElem = driver.findElement(By.xpath("//*[@id=\"id_queue\"]/option[2]"));
+    private WebElement summary = driver.findElement(By.xpath("//*[@id=\"id_title\"]"));
+    private WebElement issueDescription = driver.findElement(By.xpath("//*[@id=\"id_body\"]"));
+    private WebElement priority = driver.findElement(By.xpath("//*[@id=\"id_priority\"]"));
+    private WebElement priorityCritical = driver.findElement(By.xpath("//*[@id=\"id_priority\"]/option[1]"));
+    private WebElement dueOn = driver.findElement(By.xpath("//*[@id=\"id_due_date\"]"));
+    private WebElement eMailAddress = driver.findElement(By.xpath("//*[@id=\"id_submitter_email\"]"));
+    private WebElement submitTicket = driver.findElement(By.xpath("//*[@id=\"content-wrapper\"]/div/div/div/div[2]/form/button"));
 
+    public void newTicket(String summary, String issueDescription, String eMailAddress) {
+        queue.sendKeys(Keys.ENTER);
+        queueElem.click();
+        this.summary.sendKeys(summary);
+        this.issueDescription.sendKeys(issueDescription);
+        priority.click();
+        priorityCritical.click();
+        dueOn.sendKeys(Keys.ENTER);
+        this.eMailAddress.sendKeys(eMailAddress);
+        submitTicket.click();
+    }
 }
--- src/test/java/HelpdeskUITest.java
+++ src/test/java/HelpdeskUITest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import pages.AbstractPage;
 import pages.LoginPage;
+import pages.TicketsPage;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
@@ -34,41 +35,18 @@ public class HelpdeskUITest {
     public void createTicketTest() {
         driver.get(System.getProperty("site.url"));
 
-        //Создание New Ticket
-        WebElement element = driver.findElement(By.xpath("//*[@id=\"wrapper\"]/ul/li[2]/a"));
-        element.click();
-        //Queue
-        element = driver.findElement(By.xpath("//*[@id=\"id_queue\"]"));
-        element.sendKeys(Keys.ENTER);
-        element = driver.findElement(By.xpath("//*[@id=\"id_queue\"]/option[2]"));
-        element.click();
-        //Summary of the problem
-        element = driver.findElement(By.xpath("//*[@id=\"id_title\"]"));
-        element.sendKeys("ui-test");
-        //Description of your issue
-        element = driver.findElement(By.xpath("//*[@id=\"id_body\"]"));
-        element.sendKeys("No comments");
-        //Priority
-        element = driver.findElement(By.xpath("//*[@id=\"id_priority\"]"));
-        element.click();
-        element = driver.findElement(By.xpath("//*[@id=\"id_priority\"]/option[1]"));
-        element.click();
-        //Due on
-        element = driver.findElement(By.xpath("//*[@id=\"id_due_date\"]"));
-        element.sendKeys(Keys.ENTER);
-        //Your E-Mail Address
-        element = driver.findElement(By.xpath("//*[@id=\"id_submitter_email\"]"));
-        element.sendKeys("email@gmail.com");
-        //Submit Ticket
-        element = driver.findElement(By.xpath("//*[@id=\"content-wrapper\"]/div/div/div/div[2]/form/button"));
-        element.click();
+//        //Создание New Ticket
+        WebElement newTicket = driver.findElement(By.xpath("//*[@id=\"wrapper\"]/ul/li[2]/a"));
+        newTicket.click();
+        TicketsPage ticketsPage = new TicketsPage();
+        ticketsPage.newTicket("ui-test", "No comments", "email@gmail.com");
 
         //Log in
         driver.findElement(By.xpath("//*[@id=\"userDropdown\"]")).click();
         LoginPage loginPage = new LoginPage(driver);
         loginPage.login(System.getProperty("user"), System.getProperty("password"));
 
-//        Закрываем текущее окно браузера
-        //driver.close();
+//      //Закрываем текущее окно браузера
+        driver.close();
     }
 }
